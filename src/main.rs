1ceb59f8ff17771b22a75644124ba329d9143c46
Nahianether <nahianether3@gmail.com>
2024-02-06 11:32:34 +0600

    add fibonacci

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ println!("The value of z is: {z}");
 
 let test_sentence = string_test();
 println!("The value of test_sentence is: {test_sentence}");
+
+let fib = fibonacci(10);
+println!("The value of fib is: {fib}");
 }
 
-fn calculation(x: i32, y: i32, z: i32) -> i32 {
+fn calculation(x: i8, y: i8, z: i8) -> i8 {
    return x + y * z / x - y;
 }
 
@@ -14,9 +17,20 @@ fn string_test()-> String{
     let greeting: &str = "Greetings";
     let planet: &str = "🪐";
 
+   if greeting.is_empty() {
+      return greeting.to_string();
+   }
+
     let mut test_sentence = String::new();
     test_sentence.push_str(greeting);
     test_sentence.push_str(" from ");
     test_sentence.push_str(planet);
-    return test_sentence;
+    test_sentence
+}
+
+fn fibonacci(n: i32) -> i32 {
+    if n <= 1 {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
