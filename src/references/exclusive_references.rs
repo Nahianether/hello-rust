3495fe98b8871e8bd97456d79381af7247ee7a51
Nahianether <nahianether3@gmail.com>
2024-02-25 14:17:40 +0600

    exclusive reference example

--- src/references/exclusive_references.rs
+++ src/references/exclusive_references.rs
@@ -1,3 +1,6 @@
 pub fn exclusive_references_fn(){
-    println!("exclusive references:");
+    let mut point = (1, 2);
+    let x_coord = &mut point.0;
+    *x_coord = 20;
+    println!("exclusive references: {point:?}");
 }
